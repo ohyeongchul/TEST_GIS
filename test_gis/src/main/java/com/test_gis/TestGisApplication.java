50383ed0cecd2af53057d1b241c30ddd67ec2f32
oh <rs507@naver.com>
2021-04-29 13:26:10 +0900

    test1

---
+++ test_gis/src/main/java/com/test_gis/TestGisApplication.java
@@ -0,0 +1,13 @@
+package com.test_gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGisApplication.class, args);
+	}
+
+}
